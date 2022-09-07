0432e14b20c872eb0713b1aeaf1558acdbcfd72c
Murilo Messias de Camargo <murilo.messias@gmail.com>
2022-09-07 09:58:08 -0300

    refactor: clean up default nextjs files

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Boilerplate NextJS</title>
+      </Head>
+
+      <main>
+        <h1>Boilerplate NextJS + Eslint & Prettier</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
